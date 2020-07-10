5590e52f10154694ad243c77f7ad31045641a40f
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-10 15:49:50 -0300

    ajustes na chamada do componente movie list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data.js';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <div className='movieList'>
-        <MovieList />
+        <MovieList movies={movies}/>
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,10 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
-import movies from './data.js';
 
 class MovieList extends React.Component {
   render() {
+    const movies = this.props.movies;
     const list = movies.map((movie) => {
       return <MovieCard movie={movie} key={movie.title} />;
     });
