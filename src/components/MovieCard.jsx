d4fd6f809f40cd9f2a0c8400c5133c3cdc2adb23
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-10 15:19:55 -0300

    implmentaçao demovie card e movie list

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div className='movieCard'>
+        <div className='movieTitle'>{title}</div>
+        <div className='movieSubtitle'>{subtitle}</div>
+        <div className='movieStory'>{storyline}</div>
+        <div className='movieImage'>{imagePath}</div>
+        <div className='movieRating'>{rating}</div>
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,13 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
+import movies from './src/data.js'
 
 class MovieList extends React.Component {
   render() {
-    return
+    return movies.map(movie => {
+      <MovieCard movie={movie} key={movie.title} />
+    });
   }
 }
 
