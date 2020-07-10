6e7ba429d6a66070c301f4e1c38704ba9eede4fe
Mau <mauricio.sdjr@gmail.com>
2020-07-10 17:23:22 -0300

    implement and render MovieCard and MovieList components with their images

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return ( 
+      <p>
+        {title}
+        <img src={imagePath} alt={title} />
+      </p>
+    );
+  }
+}
+
+export default MovieCard;
+
+
+
+
+
