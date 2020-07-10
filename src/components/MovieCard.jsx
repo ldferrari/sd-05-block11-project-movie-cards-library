2718687c68f1eb619995dea0c4410ced2153fdbe
RenataNovais <rnovaiscs@gmail.com>
2020-07-10 19:56:51 -0300

    fixing cc

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,6 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        
         {rating}
         <img alt={`${title} movie`} src={imagePath} />
       </div>
