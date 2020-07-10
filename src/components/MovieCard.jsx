1ebc2797f0480aa3dee70f9014bb914a7afae6d8
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-10 16:17:52 -0300

    utilizaçao das classes fornecidas

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,14 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div className="movieCard">
-        <div className="movieTitle"><h4>{title}</h4></div>
-        <div className="movieSubtitle"><h5>{subtitle}</h5></div>
-        <div className="movieStory"><p>{storyline}</p></div>
-        <img src={imagePath} alt={title} className="movieImage" />
-        <div className="movieRating"><Rating rating={rating} /></div>
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <div className="movie-card-title"><h4>{title}</h4></div>
+          <div className="movie-card-subtitle"><h5>{subtitle}</h5></div>
+          <div className="movie-card-storyline"><p>{storyline}</p></div>
+          <img src={imagePath} alt={title} className="movie-card-image" />
+          <div className="movie-card-rating"><Rating rating={rating} /></div>
+        </div>
       </div>
     );
   }
