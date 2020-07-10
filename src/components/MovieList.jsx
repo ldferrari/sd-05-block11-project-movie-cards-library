e68164a21326f7a4ebf5953d06a561c9580f9a5c
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-10 19:24:28 -0300

    trocando map por forEach

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    return movies.map((movie) => (
+    return movies.forEach((movie) => (
       <MovieCard
         title={movie.title}
         subtitle={movie.subtitle}
