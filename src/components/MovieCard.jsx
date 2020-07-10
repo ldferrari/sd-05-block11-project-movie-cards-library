4748c37147569765dea06604b2a4760703e4f40d
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-10 18:16:03 -0300

    Criando componente MovieCard.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline } = movie;
     return (
       <div>
-        filme
+        <img src={imagePath} alt={title} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
       </div>
     );
   }
