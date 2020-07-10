3b8ca9a6d6d255c355d8088564cdd8b75be3964e
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-10 15:57:42 -0300

    Melhorando o MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
 
+    return (
+      <div>
+        {movies.map((movie, index) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
+    );
+  }
 }
+
+export default MovieList;
