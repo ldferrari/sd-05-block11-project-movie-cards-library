12e7a13b37c3b57e16cb9914cb71c77797e78a0d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-10 15:18:03 -0300

    fixed CC for 2 first requirements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 // implement Header component here
-import React from 'react'
+import React from 'react';
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
       <header>
         <h1>Movie Cards Library</h1>
       </header>
-    )
+    );
   }
 }
 
