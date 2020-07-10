718b9f38bc346e6e69df11c43dce532c254c18fe
RenataNovais <rnovaiscs@gmail.com>
2020-07-10 20:05:17 -0300

    finishing project! lets see codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -9,7 +10,7 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        {rating}
+        <Rating rating={rating} />
         <img alt={`${title} movie`} src={imagePath} />
       </div>
     );
