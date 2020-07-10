76a9411d74d5f2d566a4b2d252a9db7af79f493e
Marylange <marylangess@gmail.com>
2020-07-10 19:06:36 -0300

    codeclimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        {movies.map((elemento, index) => <MovieCard key={index} cards={elemento} />)}
+        {movies.map((elemento) => <MovieCard key={elemento.title} cards={elemento} />)}
       </div>
     );
   }
