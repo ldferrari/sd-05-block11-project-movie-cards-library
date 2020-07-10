ad3ff898db3f3b0f2e8353a9db76519aba67502d
luiz-avelar <luiz.avelar25@gmail.com>
2020-07-10 15:13:27 -0300

    Requirement 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,18 @@
 // implement Header component here
+
+// 2 Renderize o texto "Movie Cards Library" dentro de Header
+// O texto deverá estar dentro de uma tag h1, que por sua vez deve estar dentro de uma tag header.
+
+import React from 'react'
+
+class Header extends React.Component{
+  render () {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
