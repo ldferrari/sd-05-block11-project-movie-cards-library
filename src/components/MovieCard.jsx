0a45d58ffbdededf13ef456fddade1c0171f1002
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-10 18:26:28 -0300

    Criando o componente Rating, importando no Movie Card.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline } = movie;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
       <div>
         <img src={imagePath} alt={title} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={rating} />
       </div>
     );
   }
