e17881df9336c7745137871839844036f7a1bcb8
fabiano Alves <fasnaweb2020@gmail.com>
2020-07-10 17:20:59 -0300

    Organizando erros

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React,{Component} from 'react'
-import '../App.css'
+import React, { Component } from 'react';
+import '../App.css';
 
 export default class CabecalhoHeader extends Component {
-    render(){
-      return <header className="movie-card-header">
-               <h1>Movie Cards Library</h1>
-             </header>    
-    }
+  render() {
+    return (
+      <header className='movie-card-header'>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
