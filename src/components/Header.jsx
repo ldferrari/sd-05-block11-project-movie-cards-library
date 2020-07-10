22af9848401c027ab9987419b56ea3d1ce64c36d
fabiano Alves <fasnaweb2020@gmail.com>
2020-07-10 17:25:33 -0300

    Organizando codigo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 export default class CabecalhoHeader extends Component {
   render() {
     return (
-      <header className='movie-card-header'>
+      <header className="movie-card-header">
         <h1>Movie Cards Library</h1>
       </header>
     );
