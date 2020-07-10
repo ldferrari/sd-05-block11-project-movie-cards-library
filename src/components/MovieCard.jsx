00144e5d4c9a6b8a18f500f8481ebda83608045d
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-10 18:09:58 -0300

    3a correcao codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div>
         <img src={imagePath} alt={`${title}`} />
