416777993a2b14a2c13abb90e9a8da3a90d0f7d7
Rafael Assad <rafaassadeng@gmail.com>
2020-07-10 18:20:15 -0300

    single quote pode não zé

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,11 +5,11 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
-      <div className='movie-card'>
-        <h2 className='movie-card-title'>{title}</h2>
-        <h3 className='movie-card-subtitle'>{subtitle}</h3>
-        <p className='movie-card-storyline'>{storyline}</p>
-        <img src={imagePath} className='movie-card-image' />
+      <div className="movie-card">
+        <h2 className="movie-card-title">{title}</h2>
+        <h3 className="movie-card-subtitle">{subtitle}</h3>
+        <p className="movie-card-storyline">{storyline}</p>
+        <img src={imagePath} className="movie-card-image" />
       </div>
     );
   }
