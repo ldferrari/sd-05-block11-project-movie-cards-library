e1a3c2aac3fa50d731d18af228df713beab79628
RenataNovais <rnovaiscs@gmail.com>
2020-07-10 17:21:57 -0300

    Primeiro requisito - criando o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
-// inciando o projeto :rocket: 
+// inciando o projeto :rocket:
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className='movie-card-header'>
+        Movie Cards
+      </header>
+    )
+  }
+}
+
+export default Header; 
