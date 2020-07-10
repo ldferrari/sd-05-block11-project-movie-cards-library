7b3d8b7cf3a0aa0f5ad2f2bb3fa8a6bf2d6fbab4
Rafael Assad <rafaassadeng@gmail.com>
2020-07-10 17:25:11 -0300

    titulos e containers

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+    const { title } = this.props.movie;  
+      return (
+      <div className="movie-card">
+        {title}
+      </div>
+    );
+  }
+}
+  
+  export default MovieCard;
