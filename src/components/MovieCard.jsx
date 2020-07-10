cba762dcb992c1eb7aee81a5ead28870182185f3
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-10 18:35:19 -0300

    requirements 11 to 14 solved

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
         <img src={imagePath} alt={`${title}`} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={rating} />
       </div>
     );
   }
