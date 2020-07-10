4af48a91211684527c5f27f15d2c35e6c8b39518
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-10 15:46:16 -0300

    Criaçao do componente rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div className='movieCard'>
-        <div className='movieTitle'>{title}</div>
-        <div className='movieSubtitle'>{subtitle}</div>
-        <div className='movieStory'>{storyline}</div>
+        <div className='movieTitle'><h4>{title}</h4></div>
+        <div className='movieSubtitle'><h5>{subtitle}</h5></div>
+        <div className='movieStory'><p>{storyline}</p></div>
         <img src={imagePath} alt={title} className='movieImage'></img>
-        <div className='movieRating'>{rating}</div>
+        <div className='movieRating'><Rating rating={rating} /></div>
       </div>
     );
   }
