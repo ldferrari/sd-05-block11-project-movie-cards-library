9b03a18cade46f7dfa19e77de17fb3128f5080fa
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-10 16:14:49 -0300

    funcionando agora

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,12 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={imagePath} alt={title} />
         <h4>{title}</h4>
-        <h5 className="movie-card-subtitle"> {subtitle}</h5>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={rating} />
       </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div className="movie-list">
-        {movies.map((filme) => <MovieCard movies={filme} key={filme.title} />)}
+        {movies.map((filme) => <MovieCard movie={filme} key={filme.title} />)}
       </div>
     );
   }
