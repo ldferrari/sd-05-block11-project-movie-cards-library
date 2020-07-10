eb81962bb47f321adb5877fcb5803cd7b9da3be6
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-10 17:31:12 -0300

    film card and film list added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+
+function movieCard(props) {
+  return (
+    <div>
+      <img src={props.imagePath} alt="Film card" width="300px" />
+      <h2>{props.title}</h2>
+      <h3>{props.subtitle}</h3>
+      <p>Storyline: {props.storyline}</p>
+      <p>Rating: {props.rating}</p>
+    </div>
+  );
+}
+
+export default movieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import Movie from '../data';
+import MovieCard from './MovieCard';
+
+function movieList() {
+  return (
+    <div>
+      {MovieCard(Movie[0])}
+      {MovieCard(Movie[1])}
+      {MovieCard(Movie[2])}
+      {MovieCard(Movie[3])}
+      {MovieCard(Movie[4])}
+    </div>
+  );
+}
+
+export default movieList;
