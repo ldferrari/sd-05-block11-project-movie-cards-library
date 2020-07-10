c867492b1840e752dfc4fa595c062030dcd1ee02
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-10 15:42:22 -0300

    está renderizando tudo, mas há erros nos testes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={title}/>
+        <h4>{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating rating={rating}/>
+      </div>
+    );
+  };
+}
+
+export default MovieCard;
