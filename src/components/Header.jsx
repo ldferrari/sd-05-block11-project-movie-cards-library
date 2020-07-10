698ae52c67b9fff2dfd9395cec21bb5c29c17e25
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-10 16:02:27 -0300

    Text Header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const header = () => <header><h1>Movie list</h1></header>;
+const header = () => <header><h1>Movie Cards Library</h1></header>;
 
 export default header;
