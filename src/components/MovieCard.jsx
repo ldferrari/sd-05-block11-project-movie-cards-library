92bf1db0743e1fcbf576ea7c4ad20aa9949a7a0b
marinabrcls <barcelos.marina@gmail.com>
2020-07-10 18:29:26 -0300

    criei as estruturas movieList e movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className='movie-container'>
+        <div className='movie-info'>
+          <p> Title: {this.props.movie.title} </p>
+          <p> Subtitle: {this.props.movie.subtitle} </p>
+          <p> Rating: {this.props.movie.rating} </p>
+          <p> Storyline: {this.props.movie.storyline} </p>
+        </div>
+        <div className='movie-thumbnail'>
+          <img src={this.props.movie.imagePath} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import movies from './src/data';
+import MovieCard from './components/MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className='movie-list'>
+        {movies.map(movie => <MovieCard movie={movie} />)}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
