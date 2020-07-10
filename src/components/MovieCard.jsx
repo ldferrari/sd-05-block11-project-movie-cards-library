e5033cf8435d60c37f21a2b4c824a57233474ca8
pedrohcalado <pedrocalado22@gmail.com>
2020-07-10 17:40:31 -0300

    Everything

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div key={this.props.key}>
+      <div>
         <img src={imagePath} alt={title} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
