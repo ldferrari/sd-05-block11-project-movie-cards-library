6d1136af84b163f4a15f2940020518d219bc41c5
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-10 19:48:14 -0300

    codeclimate hotfix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,12 +4,12 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const {movies} = this.props;
-    return(
-      <div className='movie-list'>
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
         {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
       </div>
-    )
+    );
   }
 }
 
