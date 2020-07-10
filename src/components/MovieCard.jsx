448b57b613e038957d3afade97222860d46e1be5
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-10 19:55:40 -0300

    codeclimate hotfix

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,11 +8,11 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={imagePath} alt={`${title}`} />
-          <div className="movie-card-body">
-            <h4 className="movie-card-title" >{title}</h4>
-            <h5 className="movie-card-subtitle" >{subtitle}</h5>
-            <p className="movie-card-storyline">{storyline}</p>
-          </div>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title" >{title}</h4>
+          <h5 className="movie-card-subtitle" >{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
         <Rating className="rating" rating={rating} />
       </div>
     );
