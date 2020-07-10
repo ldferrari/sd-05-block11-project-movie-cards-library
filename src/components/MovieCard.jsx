8b1f715f64112609839a3b414fcd2c4a40de496a
Marylange <marylangess@gmail.com>
2020-07-10 19:52:13 -0300

    requisito 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,6 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <p>{rating}</p>
         <img src={imagePath} alt="Imagem dos filmes" />
       </div>
     );
