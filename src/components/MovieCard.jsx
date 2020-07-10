a1a15ed0e1a22384a9c5db6131d53d2ad41af396
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-10 15:56:07 -0300

    climate issues 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,11 +6,11 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5 className="movie-card-subtitle"> { subtitle }</h5>
-        <p className="movie-card-storyline">{ storyline }</p>
-        <Rating rating={ rating } />
+        <img className="movie-card-image" src={imagePath} alt={title} />
+        <h4>{title}</h4>
+        <h5 className="movie-card-subtitle"> {subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating rating={rating} />
       </div>
     );
   }
