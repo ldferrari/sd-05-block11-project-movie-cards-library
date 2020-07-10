225785ade5736f8d2a0b5ca4f1f1ac458a533141
RenataNovais <rnovaiscs@gmail.com>
2020-07-10 20:47:02 -0300

    adding style to my page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,14 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={rating} />
-        <img alt={`${title} movie`} src={imagePath} />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" alt={`${title} movie`} src={imagePath} />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <div className="movie-card-rating"><Rating rating={rating} /></div>
+        </div>
       </div>
     );
   }
