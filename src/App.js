aa94f40c19b8dd254ba79afdcf038897e6b3150f
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-10 19:37:00 -0300

    all requisites done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import MovieList from './components/MovieList'
+import movie from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Header />
+    <MovieList movies={movie} />
     </div>
   );
 }
