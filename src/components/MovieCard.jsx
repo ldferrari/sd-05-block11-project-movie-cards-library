d4a845aa4a2ec75c5e1cc232238d3c97400ad535
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-10 18:05:15 -0300

    Criando o component MovieCard - esqueleto MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,2 +1,23 @@
 // implement MovieCard component here
 import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+      return(
+        <div className='movie-container'>
+          <div className='movie-details'>
+            <p> Title: { this.props.movie.title } </p>
+            <p> Subtitle: { this.props.movie.subtitle } </p>
+            <p> Rating: { this.props.movie.rating } </p>
+            <p> StoryLine: { this.props.movie.storyline } </p>
+          </div>
+          <div className='movie-img'>
+            <img src={ this.props.movie.imagePath } />
+          </div>
+        </div>
+
+      )
+    }
+}
+
+export default MovieCard;
