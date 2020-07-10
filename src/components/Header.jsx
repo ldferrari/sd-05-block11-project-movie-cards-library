bc072aabc9fdfacafca308586a8ac2e452170766
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-10 17:18:29 -0300

    1-2 requirement completed - implement header and title h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>MOVIE CARDS LIBRARY</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
