365ae39b3d0a187b34992bf452cd9399911a0a9b
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-10 17:01:46 -0300

    requirement 7 img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,16 @@
 // implement MovieCard component here
-
 import React from 'react';
 
+// 6. MovieCard recebe prop movie, objeto contendo propriedades title, subtitle, storyline, imagePath e rating.
+// 7. Renderiza tag img, src com valor da propriedade imagePath.
+
 class MovieCard extends React.Component {
-  // render() {
-  //   return (
-  //   );
-  // }
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <img src={this.props.movie.imagePath}></img>
+    );
+  }
 }
 
 export default MovieCard;
