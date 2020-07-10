4512502d8271184682800f6cb6d52c083f54e5e8
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-10 18:58:01 -0300

    add MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <section className="movies">
+        <div>
+          <p>{this.props.title}</p>
+          <p>{this.props.subtitle}</p>
+          <p>{this.props.storyline}</p>
+        </div>
+        <div>
+          <img src={this.props.imagePath}/>
+          <p>{this.props.rating}</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,20 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    return (
-    <li>{this.props.movies}</li>
-    );
+    return movies.map(movie => (
+      <MovieCard
+      title={movie.title}
+      subtitle={movie.subtitle}
+      storyline={movie.storyline}
+      imagePath={movie.imagePath}
+      rating={movie.rating}
+      />
+    ));
   }
 }
+
+export default MovieList;
