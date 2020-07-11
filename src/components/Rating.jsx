f0cc93532671677713175ce4c2d0c4ca837400ca
fabiano Alves <fasnaweb2020@gmail.com>
2020-07-11 16:17:56 -0300

    reorganizando

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+
+import React, {Component} from 'react'
+
+export default class Rating extends Component {
+    render(){
+        const { rating } = this.props;
+        return (
+        <div className="rating">
+            {rating}
+        </div>            
+        )
+    }
+} 
