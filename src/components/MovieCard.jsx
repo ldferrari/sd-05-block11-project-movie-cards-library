846bff6593a9c2707de0a032a71a8d067b25d0ed
Marylange <marylangess@gmail.com>
2020-07-11 12:24:48 -0300

    visual da página

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,18 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div>
-        <img src={imagePath} alt="card de filmes" />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <div>{<Rating rating={rating} /> }</div>
+      <div className="card">
+        <div>
+          <img src={imagePath} className="image" alt="card de filmes" />
+        </div>
+        <div className="container">
+          <h4 className="title">{title}</h4>
+          <h5 className="subtitle">{subtitle}</h5>
+          <p className="description">{storyline}</p>
+        </div>
+        <div className="evaluation">
+          <div className="rating">{<Rating rating={rating} />}</div>
+        </div>
       </div>
     );
   }
