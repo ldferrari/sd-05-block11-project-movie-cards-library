108013dadd80fb54c3090e6fa163521988600bf7
Marylange <marylangess@gmail.com>
2020-07-11 09:23:31 -0300

    atualiando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div>
-        <img src={imagePath} alt="Imagem dos filmes" />
+        <img src={imagePath} alt="Imagens dos jogos" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
