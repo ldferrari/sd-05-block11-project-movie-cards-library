ae5929994fd56b7f11e3d998f167174d92af81bd
ambrosio <matmagamb1@gmail.com>
2020-07-10 22:05:55 -0300

    new push ja perdia a conta2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <section >
-      <MovieList movies={Filmes} />
+        <MovieList movies={Filmes} />
       </section>
     </div>
   );
