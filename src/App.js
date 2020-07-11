0a1961a8dce8035ec05d6c29b5d7bd3a14d0faee
Marylange <marylangess@gmail.com>
2020-07-11 09:07:32 -0300

    atualiando

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Library from './data';
 import Header from '../src/components/Header';
 import MovieList from '../src/components/MovieList';
+import Rating from '../src/components/Rating';
 
 function App() {
   return (
     <div className="App">
       {<Header /> }
       {<MovieList movies={Library} /> }
+      {<Rating /> }
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../App.css';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div>
+        <img src={imagePath} alt="Imagem dos filmes" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <img src={imagePath} alt="Imagem dos filmes" />
+        <div>
+          {<Rating rating={rating} /> }
+        </div>
       </div>
     );
   }
