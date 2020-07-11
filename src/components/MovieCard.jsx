c51a4f2ad54f45fd73027e411cf36de2e84959ee
marinabrcls <barcelos.marina@gmail.com>
2020-07-10 21:06:52 -0300

    fiz o restante dos requisitos e passei nos testes, vamos ver codeclimate agora

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,24 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div className="movie-container">
+      <section className="movie-container">
         <div className="movie-thumbnail">
           <img src={imagePath} alt={title} />
         </div>
         <div className="movie-info">
-          <p> Title: {title} </p>
-          <p> Subtitle: {subtitle} </p>
-          <p> Rating: {rating} </p>
-          <p> Storyline: {storyline} </p>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
         </div>
-      </div>
+        <div className="movie-rating">
+          <Rating rating={rating} />
+        </div>
+      </section>
     );
   }
 }
