238761aa673f5fc60072ed9c37443c087573fd15
ferrari <luisdiasferrari@gmail.com>
2020-07-11 01:23:46 -0300

    Requirements done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{title}</h4>
         <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
-        <div className="rating">{rating}</div>
+        <Rating rating={rating} />
       </div>
     );
   }
