beb72f79f33ea4f13de266f0100553f5aa07fb19
marinabrcls <barcelos.marina@gmail.com>
2020-07-10 21:07:36 -0300

    prettier

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,16 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <section className="movie-container">
-        <div className="movie-thumbnail">
+      <section className='movie-container'>
+        <div className='movie-thumbnail'>
           <img src={imagePath} alt={title} />
         </div>
-        <div className="movie-info">
+        <div className='movie-info'>
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
           <p>{storyline}</p>
         </div>
-        <div className="movie-rating">
+        <div className='movie-rating'>
           <Rating rating={rating} />
         </div>
       </section>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,8 +6,10 @@ class MovieList extends React.Component {
   render() {
     const movies = this.props.movies;
     return (
-      <div className="movie-list">
-        {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      <div className='movie-list'>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.title} />
+        ))}
       </div>
     );
   }
