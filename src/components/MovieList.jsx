e3300c69b8db7de501277e54487f2efdc1ad9bd9
fabiano Alves <fasnaweb2020@gmail.com>
2020-07-12 16:45:26 -0300

    Reorganizando Projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 export default class ConteinerCard extends Component {
   render() {
-    const { movies } = this.props;
-    return (
-      <div className='movie-list'>
+      const { movies } = this.props;
+      return (
+      <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard ky={movie.title} movie={movie} />
         ))}
