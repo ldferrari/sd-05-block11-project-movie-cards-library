5638c21132a964122e657333819d7d987409f5eb
Andre V <andrevezi@gmail.com>
2020-07-12 18:05:45 -0300

    Commit 3 - big one , added movielist / card , imagepath at imp tag , titlet at h4 .

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div>
+        <strong><h4>{title}</h4></strong>,
+      
+        made by <em>{subtitle}</em>,
+        sold {storyline} units
+        tested by {rating}
+        with <img src={imagePath} />
+    
+    </div>
+    );
+  }
+}
+
+export default MovieCard;
