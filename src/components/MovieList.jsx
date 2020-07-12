2fbd15af914dcdf38eddc5c0c299f001cf46e05c
Preto115 <preto115@gmail.com>
2020-07-12 18:25:24 -0300

    requesitos App sendo implementados

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = [this.props.movies];
+    return (
+      <section>
+        {movies.map((movie, index) => {
+          const lista = <MovieCard key={movie[index].title} movie={movie} />;
+          return lista;
+        })}
+      </section>
+    );
+  }
+}
+export default MovieList;
