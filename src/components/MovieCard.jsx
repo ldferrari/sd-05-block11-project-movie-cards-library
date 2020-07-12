2b41780100c1e09ea6894a5148def101b0028ae0
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-12 12:28:23 -0300

    Add title, subtitle, storyline, rating - MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,20 +4,21 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
-      return (
-        <div className="movie-container">
-          <div className="movie-details">
-            <p> Title: { this.props.movie.title } </p>
-            <p> Subtitle: { this.props.movie.subtitle } </p>
-            <p> Rating: { this.props.movie.rating } </p>
-            <p> StoryLine: { this.props.movie.storyline } </p>
-          </div>
-          <div className="movie-img">
-            <img src={imagePath} alt={title} />
-          </div>
+    return (
+      <div className="movie-container">
+        <div className="movie-img">
+          <img src={imagePath} alt={title} />
         </div>
-
-      )
+        <div className="movie-details">
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+        <div className="rating-movie">
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
   }
 }
 export default MovieCard;
