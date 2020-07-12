5ffe737fa97a41165bdc0aad02e726878b9a68e8
fabiano Alves <fasnaweb2020@gmail.com>
2020-07-12 16:49:21 -0300

    Reorganizando Projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,7 @@
-
-import React, {Component} from 'react'
-
+import React, { Component } from 'react';
 export default class Rating extends Component {
-    render(){
-        const { rating } = this.props;
-        return (
-        <div className="rating">
-            {rating}
-        </div>            
-        )
-    }
-} 
+  render() {
+    const { rating } = this.props;
+    return <div className="rating">{rating}</div>;
+  }
+}
