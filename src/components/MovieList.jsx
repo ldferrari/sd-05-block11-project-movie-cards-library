c526ec6a2c3c8c3d2db175de03b639e51e6ffb15
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-12 12:11:04 -0300

    corrigindo code climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        {movies.map(movie => <MovieCard movie={movie} />)}
+        {movies.map((movie) => (<MovieCard movie={movie} />))}
       </div>
     );
   }
