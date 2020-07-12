8c30c05f1b61e675df9b37b7926286a5a43b6fb8
fabiano Alves <fasnaweb2020@gmail.com>
2020-07-12 17:25:58 -0300

    Reorganizando Projeto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Dados from './data'
-import MovieCard from './components/MovieCard'
-import MovieList from './components/MovieList'
-
+import Dados from './data';
+import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-      <Header />;     
-      <MovieList movies={Dados} />
+      <Header/>;
+      <MovieList movies={Dados} />;
     </div>
   );
 }
