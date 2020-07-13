11c9b3aa667b6b6e3723b831c9ec85d1dc33e009
César Alves <alvescesar@hotmail.com>
2020-07-13 02:12:38 -0300

    Rendered all components + fixed failed npm tests

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,8 +8,8 @@ class MovieCard extends React.Component {
     return (
       <div>
         <img src={imagePath} alt={title} />
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <Rating rating={rating} />
       </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const myMovies = this.props.movies;
+
+    return (
+      <div>
+        {myMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
