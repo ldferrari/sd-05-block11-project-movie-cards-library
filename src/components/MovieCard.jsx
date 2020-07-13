4c21ff7361c90553911747ae3b0c7de1d7f2bc76
Andre V <andrevezi@gmail.com>
2020-07-12 23:03:25 -0300

    Commit 5 - Added and tested rating part

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div>
-        <strong><h4>{title}</h4></strong>,
+        <strong><h4>{title}</h4></strong>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        tested by {rating}
-        with <img src={imagePath} alt={title} />
+        <img src={imagePath} alt={title} />
+        <Rating rating={rating} />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    const divstl = {
+      width: '10%',
+      height: '10%',
+    };
+    return (
+      <div className="rating" style={divstl}> {rating} </div>
+    );
+  }
+
+}
+export default Rating;
