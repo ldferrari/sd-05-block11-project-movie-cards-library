af5085ec57d82621899ca0d6595c31bbf62ac011
Erlingsjunior <erlingsjunior@gmail.com>
2020-07-12 21:14:09 -0300

    quarto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 //class nomeDoComponete extends React.Component{render(){}}
 class Header extends React.Component {
   render() {
-  return (
-    <h1>Batatinhas</h1>
-  );
- }
+    return (
+      <h1>Batatinhas</h1>
+    );
+  }
 }
 
 export default Header;
