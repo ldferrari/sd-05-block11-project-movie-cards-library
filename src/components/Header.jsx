65218d050782da221fe2d524fdca1b85f2a8611d
Erlingsjunior <erlingsjunior@gmail.com>
2020-07-12 21:03:55 -0300

    segundo commit codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 //class nomeDoComponete extends React.Component{render(){}}
-class Header extends React.Component{
-    render() {
-        
-    }
+class Header extends React.Component {
+ render() {
+  return (batatinha);
+ }
 }
 
 export default Header;
