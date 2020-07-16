e7d01646e34f06161a3e5269b977f85d54033cfe
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-07-16 00:20:56 -0300

    correçao cc MovieList|done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        const Movies = this.props.movies;
-        return (
-            <div className="movie-list">
-                {Movies.map((each) => <MovieCard movie={each} key={each.title} />)}
-            </div>
+  render() {
+      const Movies = this.props.movies;
+      return (
+       <div className="movie-list">
+              {Movies.map((each) => <MovieCard movie={each} key={each.title} />)}
+       </div>
         );
     }
 }
