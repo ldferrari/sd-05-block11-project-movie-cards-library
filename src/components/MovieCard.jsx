c9a599b27b78369a57a52e900cd5650d5c8813a4
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-16 02:35:18 -0300

    Nem ideia do porque estáá falhando renderizar Header e MovieList mas renderiza Ratings..

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from '../../../sd-05-block12-project-movie-card-library-stateful/src/components/Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+
+    return (
+      <div className="movie-card">
+        <img src={imagePath} alt={title} className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
