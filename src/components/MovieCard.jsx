f1bad0e3fad5993dd9d8f6d2db32f7edc3a750d8
Sidnei Ramos <sidneiramos02@gmail.com>
2020-07-16 07:05:27 -0300

    Corrigindo CC, mas ainda falta reparar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
     return (
       <div>
-        <img src={imagePath} />
+        <img src={imagePath} alt={title} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
