f7df3024c18716a9bc93983781e5166b4f431d97
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-07-16 00:46:17 -0300

    Renderize componentes MovieCard dentro de MovieList~|done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
-import data from './data';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import data from "./data";
+import MovieList from "./components/MovieList";
 
 function App() {
   return (
