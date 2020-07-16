5da52dc09b5e2dea94c323dcfe8329abd4ce8f19
marinabrcls <barcelos.marina@gmail.com>
2020-07-16 18:27:52 -0300

    faltavam alguns className dentro dos componentes e por isso CSS nao tava funcionando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,21 +4,20 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <section className="movie-container">
-        <div className="movie-thumbnail">
-          <img src={imagePath} alt={title} />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img src={imagePath} alt={title} className="movie-card-image" />
+          <div className="movie-card-title"><h4>{title}</h4></div>
+          <div className="movie-card-subtitle"><h5>{subtitle}</h5></div>
+          <div className="movie-card-storyline"><p>{storyline}</p></div>
+          <div className="movie-card-rating">
+            <div className="rating-text">Rating &nbsp;</div>
+            <Rating rating={rating} />
+          </div>
         </div>
-        <div className="movie-info">
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
-        </div>
-        <div className="movie-rating">
-          <Rating rating={rating} />
-        </div>
-      </section>
+      </div>
     );
   }
 }
