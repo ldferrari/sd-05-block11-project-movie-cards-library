44048f9d849b239fab8992597c3ab5cd6afbee31
labelle <beelsdelonge@gmail.com>
2020-07-16 09:06:28 -0300

    Fazendo o Header com Movie Cards Library dentro de h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+    return (
+    <header>
+        <h1>Movie Cards Library</h1>
+        <h2>Make you're choice and have some fun!</h2>
+    </header>
+    )
+    }
+};
+export default Header;
