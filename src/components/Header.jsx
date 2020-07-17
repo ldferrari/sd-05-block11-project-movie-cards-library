19956bb6696fb8ef0ab294adae30c0b789916be3
fomes <contato.felipeluna@gmail.com>
2020-07-17 10:59:21 -0300

    code climate fix -1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 class Header extends React.Component {
   return () {
     return (
-
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
+
+export default Header;
