a0ee0649d8f1ddcf068524669e66636983958296
luca <luucadecastro@gmail.com>
2020-07-17 12:42:01 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Movies from './data';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header title='Movie Cards Library' />     
+        {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      {/* deve renderizar Header e MovieList passando como prop Movies a lista contida no arquivo data.js, importando data.js no App.js */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    // deve receber prop movies
+    // Movielist deve renderizar MovieCard para cada objeto contido no array do prop movies
+    // Deve utilizar função map (renderização dinâmica) p/ renderizar uma lista. Cada componente MovieCard deve receber porp key com nome do filme.
+
+  }
+}
+
+export default MovieList;
