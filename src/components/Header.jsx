050f5208ec09a3be015270adc36916f740b664a8
SamuelPPlab <samuelpp.s@hotmail.com>
2020-07-26 14:17:09 -0300

    header pronto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return <div className="header">
+       -- Movie Library Cards --
+    </div>
+  }
+}
+
+export default Header;
