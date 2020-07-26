6196701b02c4e62ad67794c9897df38c4d84cc4f
SamuelPPlab <samuelpp.s@hotmail.com>
2020-07-26 16:38:11 -0300

    code climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,9 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     return (<div className="card center">
-      <img src={this.props.movie.imagePath} alt="Irineu - disse o mago - você não sabe nem eu." width="100%" />
+      <img src={this.props.movie.imagePath} 
+      alt="Irineu - disse o mago - você não sabe nem eu." 
+      width="100%" />
       <h4>{this.props.movie.title}</h4>
       <h5>{this.props.movie.subtitle}</h5>
       <p>{this.props.movie.storyline}</p>
