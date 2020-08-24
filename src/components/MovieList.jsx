dc93926b931449dccb7dda866a50c5380e500385
Thiago <asm.thiago@gmail.com>
2020-08-23 21:45:01 -0300

    CodeClimate OK

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
 
     return (
       <div className="movie-list">
-      {movies.map((movie) =>
+        {movies.map((movie) =>
         <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
