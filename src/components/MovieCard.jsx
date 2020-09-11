443bd713dc586af5508c9ab993296df3311b694d
Cass007 <thecastieel07@gmail.com>
2020-09-11 15:48:40 -0300

    movie finalized

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+
+import React, { Component } from 'react';
+import Rating from './Rating';
+export default class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <img src={imagePath} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating} />
+      </div>
+    );
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
+      </div>
+    );
+  }
+}
